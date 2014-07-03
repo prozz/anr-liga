53c0cc1cb2b72e6fd47aa7c84a1643c9b4a58330
prozz <prozzz@gmail.com>
2014-07-03 14:54:23 +0200

    real data, new parser definition

--- src/anr_liga/core.clj
+++ src/anr_liga/core.clj
@@ -1,89 +1,82 @@
 (ns anr-liga.core
   (:use [anr-liga.util])
-  (:require [instaparse.core :as insta])
+  (:require [instaparse.core :as insta]
+            [clojure.string :as s])
   (:gen-class))
 
 (def max-games 2)
-(def allowed-players ["Kuba M.", "Pawel R.", "David C."])
 
+(def allowed-players
+     ["Mateusz K."
+      "Pawel R."
+      "Marek T."
+      "Jan S."
+      "Maciej C."
+      "Mateusz N."
+      "David C."
+      "Kuba M."
+      "Sebastian Z."
+      "Michal R."
+      "Sebastian S."
+      "Dawid S."
+      "Mikolaj K."])
+
+(def allowed-runner-ids
+     ["Gabe"
+      "Andy"
+      "Ken"
+      "Silhouette"
+      "Iain"
+      "Kate"
+      "CT"
+      "Kit"
+      "Noise"
+      "Whizzard"
+      "Reina"])
+
+(def allowed-corp-ids
+     ["NBN:MN"
+      "NBN:TWiY"
+      "HB:EtF"
+      "HB:CI"
+      "HB:ST"
+      "HB:ND"
+      "J:PE"
+      "J:RP"
+      "J:TI"
+      "J:HM"
+      "W:BaBW"
+      "W:BWBI"
+      "GRNDL"])
 
 (def parser
+  (let [quotify  (fn [x] (map #(str "'" % "'") x))
+        tokenify (fn [x] (s/join "|" x))
+        corps    (-> allowed-corp-ids   quotify tokenify)
+        runners  (-> allowed-runner-ids quotify tokenify)
+        players  (-> allowed-players    quotify tokenify)]
     (insta/parser
-      "<games> = game*
-       game = <whitespace>* date <whitespace>* players <whitespace> score whitespace* <'\\n'>*
+      (str
+       "days = day*
+       day = date <':'> <eol>? duel+
        date = #'\\d{4}-\\d{2}-\\d{2}'
-       players = player <whitespace 'vs' whitespace> player
-       player = name
-       name = word whitespace letter dot?
-       score = number <':'> number
-       <word> = #'[a-zA-Z]+'
-       <letter> = #'[a-zA-Z]'
-       number = #'[0-9]'
-       <dot> = #'\\.'
-       <whitespace> = #'\\s+'"))
-
-(defn parse-games [data]
-  (insta/transform
-     {:players list
-      :player str
-      :name str
-      :score list
-      :number read-string
-      :game interleave}
-   (parser data)))
-
-(def data "Marek T. vs Kuba M.        3:1
-    Dawid S. vs Maciej C.     2:2
-    Pawel R. vs Mikolaj K.    3:1
-    Dawid S. vs Sebastian Z.  1:3
-    Kuba M. vs David C.       1:3
-    Maciej C. vs Mateusz N.   1:3
-    Pawel R. vs David C.      2:2
-   David C. vs Kuba M.        1:3")
-
-(def example-games
-  (parse-games data))
-
-example-games
-
-(defn all-players [games]
-  (->> games
-      flatten
-      (filter string?)
-      distinct))
+       duel = players <w> score <w> (comments)? <w> games
+       players = player <w> <'vs'> <w> player
+       score = digit <':'> digit
+       games = ((game) | (game game))? <eol>?
+       game = (<w> runner-id <w> result <w> corp-id | <w> corp-id <w> result <w> runner-id <w>) <w> (comments)?
+       result = '>'|'<'|'='
+       runner-id = "runners"
+       corp-id = "corps"
+       player = "players"
+       comments = '(' #'[a-zA-Z0-9: ]'+ ')' w eol
+       digit = #'\\d'
+       w = #'\\s'*
+       eol = #'\\n'
+      "))))
 
 
-(defn count-points [games]
-  (->>
-    games
-    flatten
-    (partition 2)
-    (group-by first)
-    (map-map-values (comp (partial apply +) (partial filter number?) flatten)
-   )))
-
-
-(defn count-matches [games]
-  (->> games
-       (map (partial filter string?))
-       (map sort)
-       (group-by identity)
-       (map-map-values count)
-       (map-map-keys (partial clojure.string/join " vs "))))
-
-(defn valid-players? [games]
-  (= (sort allowed-players) (sort (all-players games))))
-
-(defn valid-matches? [games]
-  (let [m (->> (count-matches games) vals (apply max))]
-    (< m max-games)))
-
-(count-points example-games)
-(all-players example-games)
-(count-matches example-games)
-(valid-matches? example-games)
-(valid-players? example-games)
-
+(parser (slurp "resources/data.txt"))
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
